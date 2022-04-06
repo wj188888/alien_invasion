94e79f4acbd42bb1a4424aa9879541194a0a5f93
wangjie <2460665525@qq.com>
2022-04-06 23:10:16 +0800

    外形人系列+4

--- console/alien.py
+++ console/alien.py
@@ -8,7 +8,7 @@ class Alien(Sprite):
     """表示单个外形人的类"""
     def __init__(self, ai_settings, screen):
         """初始化外星人并设置其起始位置"""
-        super().__init__()
+        super(Alien, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
         # 加载外星人图像,并设置其rect属性
